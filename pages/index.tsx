c6bc89e24d24affa203da88b06f1619fad4dd77e
Lucas Frezzini <luquiitas.f@gmail.com>
2024-12-03 21:30:12 -0300

    feat: Stats section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "components/hero";
 import { FeaturesComp } from "components/features";
 import { PricingComp } from "components/pricing";
+import { StatsSection } from "components/stats";
 
 export default function MyPage() {
   return (
@@ -8,6 +9,7 @@ export default function MyPage() {
       <HeroSection></HeroSection>
       <FeaturesComp></FeaturesComp>
       <PricingComp></PricingComp>
+      <StatsSection></StatsSection>
     </div>
   );
 }
---
+++ ui/stats-card/index.tsx
@@ -0,0 +1,12 @@
+import style from "./index.module.css";
+
+export function StatsCard(props: any) {
+  const { percentage, text, info } = props;
+  return (
+    <div className={style.stats__card}>
+      <h3>{percentage}</h3>
+      <h2>{text}</h2>
+      <p>{info}</p>
+    </div>
+  );
+}
