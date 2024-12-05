28a7b5f056102b98e7f48994cacc3e6925051f2a
Ezequiel <ezequielezequiel9@gmail.com>
2024-12-04 22:57:33 -0300

    add sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import { FeaturesComp } from "components/features";
 import { PricingComp } from "components/pricing";
 import { HowWorkComponent } from "components/how-work";
 
+import { StatsSection } from "components/stats";
+import { Footer } from "components/footer";
+
 export default function MyPage() {
   return (
     <div>
@@ -10,6 +13,9 @@ export default function MyPage() {
       <FeaturesComp></FeaturesComp>
       <PricingComp></PricingComp>
       <HowWorkComponent></HowWorkComponent>
+
+      <StatsSection></StatsSection>
+      <Footer></Footer>
     </div>
   );
 }
